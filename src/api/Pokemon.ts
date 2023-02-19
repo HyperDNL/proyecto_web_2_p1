73446fb4f6fb4ae96e8bcf9ebda975828f0c52c8
Daniel López <hyperdaniel579exe@gmail.com>
2023-02-19 04:44:09 -0600

    Delete Pokemon.ts

--- src/api/Pokemon.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getPokemonRequests = async (
-  pokemon: string | number = "pikachu"
-) => await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-export const getPokemonTypesRequests = async (type: string) =>
-  await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
