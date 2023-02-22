b3cf6f36680cb6fe970f9a79619216aac9d99419
Daniel López <hyperdaniel579exe@gmail.com>
2023-02-22 00:55:40 -0600

    página de Not Found actualizada

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -18,18 +18,18 @@ export const NotFoundPage = () => {
             <h1 className="font-bold text-red-500 text-9xl">404</h1>
 
             <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-              <span className="text-red-500">Oops!</span> Page not found
+              <span className="text-red-500">Oops!</span> Página no encontrada
             </h6>
 
             <p className="mb-8 text-center text-gray-500 md:text-lg">
-              The page you’re looking for doesn’t exist.
+              La página que buscabas no existe.
             </p>
 
             <Link
               to={"/"}
               className="px-6 py-2 text-sm font-semibold text-red-500 bg-gray-200"
             >
-              Go home
+              Ir a Inicio
             </Link>
           </div>
         </div>
